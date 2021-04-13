27f6c27636faa0758f0eb168f8f30cba711b8db2
Wojciech Kalinowski <wojciechkalinowskipoczta@gmail.com>
2021-04-13 19:11:02 +0200

    branched

--- app/src/main/java/pl/kalinowski/archiwum/testowyrep/MainActivity.kt
+++ app/src/main/java/pl/kalinowski/archiwum/testowyrep/MainActivity.kt
@@ -2,10 +2,15 @@ package pl.kalinowski.archiwum.testowyrep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.opencv.core.Mat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
