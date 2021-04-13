7a361734dc3c567a58b1318a957b7db478fc3a10
Wojciech Kalinowski <wojciechkalinowskipoczta@gmail.com>
2021-04-13 19:21:24 +0200

    Wojtek

--- app/src/main/java/pl/kalinowski/archiwum/testowyrep/MainActivity.kt
+++ app/src/main/java/pl/kalinowski/archiwum/testowyrep/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+
+
+
+
 
 
 
