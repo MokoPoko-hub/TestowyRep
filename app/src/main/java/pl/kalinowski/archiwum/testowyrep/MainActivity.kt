ae7ba86f76aa7b6224d4bd1b1656d71c0dd02219
Wojciech Kalinowski <wojciechkalinowskipoczta@gmail.com>
2021-04-26 14:53:05 +0200

    Wojtek

--- app/src/main/java/pl/kalinowski/archiwum/testowyrep/MainActivity.kt
+++ app/src/main/java/pl/kalinowski/archiwum/testowyrep/MainActivity.kt
@@ -4,56 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        fun BartekMetoda()
-        {
-            System.out.println("Elo");
-        }
-
-        fun funkcja1()
-        {
-            System.out.println("Elo");
-        }
-
-        fun funkcja2()
-        {
-            System.out.println("Elo");
-        }
-
-        fun funkcja3()
-        {
-            System.out.println("Elo");
-        }
-
-        funkcja4();
-
         print("wojtek")
+
+
+
+        metodaWojtek()
+
+
     }
 
-    fun funkcja4()
-    {
-        System.out.println("Elo");
-    }
 
-    fun funkcja5()
-    {
-        System.out.println("Elo");
-    }
+
+
 
     fun wojtekMetoda(){
         print("Elo")
-
-
-        print("wojtek")
     }
 
 
 
-
+    fun metodaWojtek(){
+        print("Wojtek")
+    }
 
 
 }
